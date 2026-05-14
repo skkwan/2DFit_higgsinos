a77f2b91e812ece86e7b7176b0bd668eae538e30
skkwan <skwan@princeton.edu>
2026-05-14 21:45:37 +0200

    replace with log function

--- hbb_zll/pdf_fit/obtain_background_components.py
+++ hbb_zll/pdf_fit/obtain_background_components.py
@@ -17,8 +17,8 @@ def plotMETFit(rooVar, dataset, pdf, dataLabel, fitLabel, plotname, outdir=""):
 
     frame = rooVar.frame(nBins)
 
-    leg = CMS.cmsLeg(0.5, 0.89 - 0.05 * 3, 0.9, 0.89, textSize=0.03)
-    leg.SetHeader("Z(ll)H(bb): MET fit")
+    leg = CMS.cmsLeg(0.3, 0.89 - 0.05 * 4, 0.9, 0.89, textSize=0.04)
+    leg.SetHeader("2018 signal region MC: background MET")
     CMS.SetLumi("")
 
     data_hist = dataset.createHistogram("histo_" + plotname, rooVar, ROOT.RooFit.Binning(nBins, xmin, xmax))
@@ -118,10 +118,8 @@ variables = ROOT.RooArgSet(mll, met, weightXyear)
 # First do NON-peaking background by itself
 bkg_nonpeak_dataset = ROOT.RooDataSet("bkg_nonpeak_dataset", "bkg_nonpeak_dataset", ROOT.RooArgSet(met, weightXyear), ROOT.RooFit.Import(bkg_nonpeak_tree), ROOT.RooFit.WeightVar(weightXyear))
 # Background fake mll component in MET dimension
-mu_nonpeak_met = ROOT.RooRealVar('mu_nonpeak_met', 'mu_nonpeak_met', 225, 100, 300)
-b_nonpeak_met = ROOT.RooRealVar('b_nonpeak_met', 'b_nonpeak_met', 40, 5, 50)
-bkg_nonpeak_met_pdf = ROOT.RooGenericPdf("bkgfakemll_met", "bkgfakemll_met", "1/b_nonpeak_met * exp(-(@0 - mu_nonpeak_met)/b_nonpeak_met - exp(-(@0 - mu_nonpeak_met)/b_nonpeak_met))",
-                        ROOT.RooArgList(met, mu_nonpeak_met, b_nonpeak_met))
+a_nonpeak_met = ROOT.RooRealVar('a_nonpeak_met', 'a_nonpeak_met', -0.005, -0.1, -0.0001)
+bkg_nonpeak_met_pdf = ROOT.RooExponential("bkgfakemll_met", "bkgfakemll_met", met, a_nonpeak_met)
 # Fit and get results
 bkg_nonpeak_result = bkg_nonpeak_met_pdf.fitTo(bkg_nonpeak_dataset, RF.Save(), RF.SumW2Error(True))
 bkg_nonpeak_params = bkg_nonpeak_met_pdf.getParameters(bkg_nonpeak_dataset)
@@ -129,28 +127,43 @@ print(bkg_nonpeak_params.Print("v"))
 
 # Then do PEAKING background by itself
 bkg_peaking_dataset = ROOT.RooDataSet("bkg_peaking_dataset", "bkg_peaking_dataset", ROOT.RooArgSet(met, weightXyear), ROOT.RooFit.Import(bkg_peaking_tree), ROOT.RooFit.WeightVar(weightXyear))
-mu_peaking_met = ROOT.RooRealVar('mu_peaking_met', 'mu_peaking_met', 50, 30, 400)   # adequate value somewhere around 246 based on hand-drawn plots
-b_peaking_met = ROOT.RooRealVar('b_peaking_met', 'b_peaking_met', 40, 20, 100)    # adequate value somewhere around 41 based on hand-drawn plots
-bkg_peaking_met_pdf = ROOT.RooGenericPdf("bkgrealmll_met", "bkgrealmll_met", "1/b_peaking_met * exp(-(@0 - mu_peaking_met)/b_peaking_met - exp(-(@0 - mu_peaking_met)/b_peaking_met))",
-                        ROOT.RooArgList(met, mu_peaking_met, b_peaking_met))
+a_peaking_met = ROOT.RooRealVar('a_peaking_met', 'a_peaking_met', -0.005, -0.1, -0.0001)
+bkg_peaking_met_pdf = ROOT.RooExponential("bkgrealmll_met", "bkgrealmll_met", met, a_peaking_met)
 # Fit
 bkg_peaking_result = bkg_peaking_met_pdf.fitTo(bkg_peaking_dataset, RF.Save(), RF.SumW2Error(True))
 bkg_peaking_params = bkg_peaking_met_pdf.getParameters(bkg_peaking_dataset)
 print(bkg_peaking_params.Print("v"))
 
+# Then do TOTAL background
+bkg_total_file = ROOT.TFile.Open('backgrounds_for_2D_fit.root', "READ")
+bkg_total_tree = bkg_total_file.Get("event_tree")
+bkg_total_dataset = ROOT.RooDataSet("bkg_total_dataset", "bkg_total_dataset", ROOT.RooArgSet(met, weightXyear), ROOT.RooFit.Import(bkg_total_tree), ROOT.RooFit.WeightVar(weightXyear))
+a_total_met = ROOT.RooRealVar('a_total_met', 'a_total_met', -0.005, -0.1, -0.0001)
+bkg_total_met_pdf = ROOT.RooExponential("bkg_total_met", "bkg_total_met", met, a_total_met)
+# Fit
+bkg_total_result = bkg_total_met_pdf.fitTo(bkg_total_dataset, RF.Save(), RF.SumW2Error(True))
+bkg_total_params = bkg_total_met_pdf.getParameters(bkg_total_dataset)
+print(bkg_total_params.Print("v"))
+
 f = ROOT.TFile("fitresult_background_MET.root", "RECREATE")
 bkg_nonpeak_result.Write("bkg_nonpeak_result")
 bkg_peaking_result.Write("bkg_peaking_result")
+bkg_total_result.Write("bkg_total_result")
 f.Close()
 
 plotMETFit(met, bkg_nonpeak_dataset, bkg_nonpeak_met_pdf,
-           "Non-peaking bkg",
-           f"Gumbel fit (#mu={mu_nonpeak_met.getVal():.1f}#pm{mu_nonpeak_met.getError():.1f}, b={b_nonpeak_met.getVal():.1f}#pm{b_nonpeak_met.getError():.1f})",
-           "bkg_nonpeak_met_gumbel")
+           "Non-peaking-in-m(ll) background",
+           f"Exponential (a = {a_nonpeak_met.getVal():.4f} #pm {a_nonpeak_met.getError():.4f})",
+           "bkg_nonpeak_met_exp")
 plotMETFit(met, bkg_peaking_dataset, bkg_peaking_met_pdf,
-           "Peaking bkg",
-           f"Gumbel fit (#mu={mu_peaking_met.getVal():.1f}#pm{mu_peaking_met.getError():.1f}, b={b_peaking_met.getVal():.1f}#pm{b_peaking_met.getError():.1f})",
-           "bkg_peaking_met_gumbel")
+           "Peaking-in-m(ll) background",
+           f"Exponential (a = {a_peaking_met.getVal():.4f} #pm {a_peaking_met.getError():.4f})",
+           "bkg_peaking_met_exp")
+plotMETFit(met, bkg_total_dataset, bkg_total_met_pdf,
+           "Total background",
+           f"Exponential (a = {a_total_met.getVal():.4f} #pm {a_total_met.getError():.4f})",
+           "bkg_total_met_exp")
 
 os.system("mv *.png /eos/user/s/skkwan/www/higgsino/studies/mll-MET-fit-2D/background_shapes")
 os.system("mv *.pdf /eos/user/s/skkwan/www/higgsino/studies/mll-MET-fit-2D/background_shapes")
+
